eaa9aa2660ac287a59accad22d3347ec72184895
unknown <ankitsingh62073@gmail.com>
2024-04-10 00:27:35 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # End-to-end-NLP-Project-Implementation
+
+## How to run?
+
+```bash
+conda create -n hate python=3.11 -y
+```
+
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="ankit62073",
+    author_email="ankitsingh62073@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
